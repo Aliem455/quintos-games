f8d297b49b10e1eb7348545711ba876c9b0c9d4f
Aliem455 <nguyen.k.nguyen031@ais.edu.vn>
2022-11-10 15:05:50 -0500

    1.1.7

---
+++ GAMES/LeafEater/leafEater.js
@@ -0,0 +1,7 @@
+new Sprite(10, 25, 16);
+new Sprite(15, 25, 16);
+new Sprite(20, 25, 16);
+
+function draw() {
+	background('#36c06c');
+}
--- load.js
+++ load.js
@@ -1,3 +1,3 @@
 QuintOS.user = 'Aliem455'; // put your github username!
 QuintOS.dir = 'GAMES'; // the folder with the game folder in it
-QuintOS.game = 'Hangman'; // the title of the game to load
+QuintOS.game = 'LeafEater'; // the title of the game to load
